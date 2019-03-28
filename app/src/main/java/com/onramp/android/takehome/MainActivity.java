f99b5eaf86f53d8572ff501a3d1711b14bbd05ad
Theda Rogers <crayonwriter@gmail.com>
2019-03-28 12:10:22 -0400

    feat: connect recyclerview to layout manager

--- app/src/main/java/com/onramp/android/takehome/MainActivity.java
+++ app/src/main/java/com/onramp/android/takehome/MainActivity.java
@@ -4,17 +4,29 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+private RecyclerView recyclerView;
+private RecyclerView.Adapter mAdapter;
+private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter= new RecyclerViewCategoriesAdapter(categories);
+        recyclerView.setAdapter(mAdapter);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
