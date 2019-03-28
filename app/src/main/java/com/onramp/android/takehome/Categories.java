84e8b7b6ba948b66ecf8d65f2d60f87bedccea02
Theda Rogers <crayonwriter@gmail.com>
2019-03-28 11:41:41 -0400

    feat: update build.gradle (Module:app) to include recyclerview implementation and create GridCategoriesAdapater

---
+++ app/src/main/java/com/onramp/android/takehome/Categories.java
@@ -0,0 +1,5 @@
+package com.onramp.android.takehome;
+
+public class Categories {
+
+}
--- app/src/main/java/com/onramp/android/takehome/GridCategoriesAdapter.java
+++ app/src/main/java/com/onramp/android/takehome/GridCategoriesAdapter.java
@@ -1,6 +1,31 @@
 package com.onramp.android.takehome;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class GridCategoriesAdapter extends BaseAdapter {
+    Context mContext;
+    Categories[] categories;
+
+    @Override
+    public int getCount() {
+        return categories.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
