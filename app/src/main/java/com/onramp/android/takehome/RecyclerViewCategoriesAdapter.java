b757513d9684c3e1eae2350066e2db834f209223
Theda Rogers <crayonwriter@gmail.com>
2019-03-31 16:21:16 -0400

    Update: modify adapter with object change to int

--- app/src/main/java/com/onramp/android/takehome/RecyclerViewCategoriesAdapter.java
+++ app/src/main/java/com/onramp/android/takehome/RecyclerViewCategoriesAdapter.java
@@ -1,18 +1,26 @@
 package com.onramp.android.takehome;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class RecyclerViewCategoriesAdapter extends RecyclerView.Adapter<RecyclerViewCategoriesAdapter.RecyclerViewCategoriesViewHolder> {
     ArrayList<Categories> mCategoriesArrayList;
     Context mContext;
+
 
     //Constructor
     public RecyclerViewCategoriesAdapter(Context context, ArrayList<Categories> arrayList) {
@@ -29,9 +37,19 @@ public class RecyclerViewCategoriesAdapter extends RecyclerView.Adapter<Recycler
 
     @Override
     public void onBindViewHolder(RecyclerViewCategoriesAdapter.RecyclerViewCategoriesViewHolder holder, int position) {
+        Context context = mContext;
+        int mDrawableId = context.getResources().getIdentifier("image", "drawable", context.getPackageName());
+
         Categories object = mCategoriesArrayList.get(position);
 
         Image image = object.getCategoryImage();
+        String webAddress = object.getCategoryClickedLocation();
+        String name = object.getCategoryName();
+
+        holder.imageView.setImageResource(mDrawableId);
+        holder.webAddressTextView.setText(webAddress);
+        holder.nameTextView.setText(name);
+
 
     }
 
@@ -40,10 +58,19 @@ public class RecyclerViewCategoriesAdapter extends RecyclerView.Adapter<Recycler
         return 0;
     }
 
-    static class RecyclerViewCategoriesViewHolder extends RecyclerView.ViewHolder {
+    public class RecyclerViewCategoriesViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView webAddressTextView;
+        TextView nameTextView;
+        Context context = mContext;
+        int mDrawableId = context.getResources().getIdentifier("image", "drawable", context.getPackageName());
+
 
         public RecyclerViewCategoriesViewHolder(View itemView) {
             super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.category_image);
+            webAddressTextView = (TextView) itemView.findViewById(R.id.category_webAddress);
+            nameTextView = (TextView) itemView.findViewById(R.id.category_text);
         }
     }
 }
